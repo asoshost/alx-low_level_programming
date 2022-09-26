a7c6bfad8eac7ac7ad8d665a0d36fbe082050b82
asoshost <asostar9117@gmail.com>
2022-09-26 10:09:52 -0700

    my codes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,23 @@
-#include "maim.h"
+#include "main.h"
 
 /**
-* _memcpy - Entry point
-* @dest: input
-* @src: input
-* @n: input
-* Return: Always 0 (Success)
-*/
+ * _memcpy - copies memory area.
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: unsigned int
+ *
+ *Return: dest
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int index;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-
+	for (index = 0; n > 0; index++)
+	{
+		dest[index] = src[index];
+		n -= 1;
+	}
 	return (dest);
 }
